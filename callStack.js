c4a26c70722396e9a833873c8fd8d098177ce53d
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-08-03 11:20:19 -0400

    Update callStack.js

--- callStack.js
+++ callStack.js
@@ -5,3 +5,14 @@ function average(a,b) {
     return add(a,b)/2;
 }
 console.log(average(5,10));
+
+for (let i = 0; i < 5; i++){
+    average(5,10);
+}
+
+
+// This is a recursive function that will lead to stack overflow
+
+// function fn() {
+//     fn();
+// }
